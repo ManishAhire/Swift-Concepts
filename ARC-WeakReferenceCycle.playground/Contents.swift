2f2469388b479145609c3cb829689088bbb2a934
Manish Ahire <manishahire@Manishs-MacBook-Pro.local>
2026-02-11 22:49:49 +0530

    ARC-Weak Reference

---
+++ ARC-WeakReferenceCycle.playground/Contents.swift
@@ -0,0 +1,55 @@
+// Weak Reference Cycle - Use to break strong reference cycle
+
+class Person {
+    
+    var name: String
+    weak var job: Job? // Always declar var and make it Optional,
+                       // Because, ARC automatically set nil value at runtime
+    
+    init(_name: String) {
+        name = _name
+        
+        print("Person => init called")
+    }
+    
+    deinit {
+        print("Person => deinit called")
+    }
+}
+
+
+class Job {
+    
+    var title: String
+    var person: Person? // Optional, because a Job may not always have a Person
+    
+    init(_title: String) {
+        title = _title
+        
+        print("Job => init called")
+    }
+    
+    deinit {
+        print("Job => deinit called")
+    }
+}
+
+
+if true {
+    
+    var objPerson: Person?
+    objPerson = Person(_name: "Manish")
+    
+    var objJob: Job?
+    objJob = Job(_title: "iOS Developer")
+    
+    objPerson!.name
+    objJob!.title
+    
+    // Strong Reference Cycle
+    objPerson!.job = objJob
+    objJob!.person = objPerson
+    
+//    objPerson = nil // But Person still poiting to Job
+//    objJob = nil // But Job still poiting to Person
+}
